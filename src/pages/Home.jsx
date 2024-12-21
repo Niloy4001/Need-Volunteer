5850a99c1a2a5f57587eb4c687d2121811e1f1ee
Niloy4001 <huzaifasikderniloy58bd@gmail.com>
2024-12-21 22:43:46 +0600

    some volunteer need posts added on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Slider from '../components/Slider'
+import VolunteerNeedsNow from '../components/VolunteerNeedsNow'
 
 const Home = () => {
   return (
     <div>
         <Slider></Slider>
+        <VolunteerNeedsNow></VolunteerNeedsNow>
     </div>
   )
 }
